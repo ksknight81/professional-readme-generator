fdfcd6e0cf3c3631345908a9b1ef0761e1c7c23a
kevin knight <ksknight81@gmail.com>
2022-07-05 22:18:20 -0400

    added list of questions

--- index.js
+++ index.js
@@ -1,7 +1,147 @@
+const inquirer = require('inquirer');
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        // users name
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+        // github account name
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username');
+                    return false;
+                }
+            }
+        },
+        // email address (for questions)
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address');
+                    return false;
+                }
+            }
+        },     
+        // project title
+        { 
+            type: 'input',
+            name: 'title',
+            message: 'What is the Title of your Project?  (Required)',
+            validate:  titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title');
+                    return false;
+                }
+            }
+        },
+        // project Description
+        {
+            type: 'input',
+            name: 'description',
+            message: 'what is the project about?',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a project description');
+                    return false;
+                }
+            }
+        },
+             
+        //installation
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install the program?',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please provide installation instructions');
+                    return false;
+                }
+            }
+        },
+        // usage
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is this program used for?',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please provide usage information');
+                    return false;
+                }
+            }
+        },
+        // license
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license(s) did you use? (Select all that apply)',
+            choices: ['Apache', 'Cloud Native Computing Foundation', 'GNU', 'npm packages', 'OpenBSD', 'Rust', 'WordPress']
+        },
+        // contributing
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please provide any contribution instructions',
+            validate: contirbutionInput => {
+                if (contirbutionInput) {
+                    return true;
+                } else {
+                    console.log('Please provide contribution instructions');
+                    return false;
+                }
+            }
+        },
+        // tests
+        {
+            type: 'input',
+            name: 'testinstructions',
+            message: 'Please enter any test instructions',
+            validate: testinstructionsInput => {
+                if (testinstructionsInput) {
+                    return true;
+                } else {
+                    console.log('Please provide test instructions');
+                    return false;
+                }
+            }
+        },
+    ])
+} ;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,4 +151,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+questions();
  
